955d1990146d6f0235f13e7c5b3fe6bf5cf3b1c8
TakisDravforSchool <pandrav2006@gmail.com>
2026-06-22 21:59:39 -0600

    Remove email credentials

--- config/settings.py
+++ config/settings.py
@@ -132,8 +132,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'northstarpropertyservices.web@gmail.com'
-EMAIL_HOST_PASSWORD = 'ucfvtwfqdpvjjkqm'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
