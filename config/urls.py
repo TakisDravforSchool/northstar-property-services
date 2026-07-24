46efde12049e53f58d365fd1b9807b47f84cee61
TakisDravforSchool <pandrav2006@gmail.com>
2026-07-24 12:56:09 -0600

    Add robots.txt and sitemap suport

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,32 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
+from django.contrib.sitemaps.views import sitemap
+from website.sitemaps import StaticViewSitemap
+
+
+
+sitemaps = {
+    "static": StaticViewSitemap,
+}
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('website.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("website.urls")),
+
+    path(
+        "robots.txt",
+        TemplateView.as_view(
+            template_name="website/robots.txt",
+            content_type="text/plain",
+        ),
+    ),
+
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
 ]
